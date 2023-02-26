5f1840a428e28a3b6f87e907506761df77dfaafb
kaka <dakongjian@163.com>
2023-02-26 22:17:32 +0800

    Create XYGAME

---
+++ Assets/Resource/ex_Res/Jiangli/DemoAssets/Scripts/MainScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// THIS SCRIPT ADDED TO THE PREFAS CAN BE USED TO CHECK THE ANIMATIONS ON THE DEMO SCENE //
+// THE SPACE OPTION NEEDS TO BE CHANGED DEPENDING ON THE ANIMATION AVAILABLE ON THE PREFAB //
+
+public class MainScript : MonoBehaviour {
+
+	private Animator animator;
+
+	void Start () {
+
+		animator = GetComponent <Animator> ();
+
+
+
+	}
+
+	void Update () {
+
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			animator.Play("anim_idle");
+
+		}
+		
+		if (Input.GetKeyDown("space")){
+
+			animator.Play("anim_open");
+			//animator.Play("anim_rotation");
+			//animator.Play("anim_play");
+		}
+
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+
+			animator.Play("anim_fall");
+		}
+
+	}
+}
